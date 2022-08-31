68623ef33f6a2412e2a2a5388b70bbe1fc30c668
Patrick Warren <patwarr@ut.utm.edu>
2022-08-31 11:18:14 -0500

    First push of Diffs and commits

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
+/**
+ * @file main.cpp
+ * @author Patrick Warren
+ * @date 2022-08-31
+ * @brief Print sum of numbers from 1 to n
+ * 
+ * Prints out the sum of numbers between 1 and the user supplied number
+ */
+
 
 #include <iostream>
 
 using namespace std;
 
+int sum(int number);
+
 int main() {
-  cout << "Hello World!" << endl;
-  
+  int n = 0;
+  cout << "Please enter a number: ";
+  cin >> n;
+ int total = sum(n);
+ cout << "The sum of the numbers from 1 to your number is: " << total << endl;
   return 0;
 }
+
+int sum (int number) {
+  int tot = 0;
+  
+  for (int i = 0; i <=  number; i++){
+    
+  tot = tot + i;
+ }
+return tot;
+}
+
